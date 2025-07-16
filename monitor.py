53dc7ef48ad07bbcdcbe78973c64b2dcbf7794cd
Shivram-S <44567173+Shivram-S@users.noreply.github.com>
2025-07-16 15:03:37 +0530

    Update monitor.py - Trying to reduce CCN again

--- monitor.py
+++ monitor.py
@@ -6,12 +6,5 @@ import sys
 def vitals_ok(temperature, pulseRate, spo2):
   if temperature > 102 or temperature < 95 or pulseRate < 60 or pulseRate > 100 or spo2 < 90:
     print('Vitals critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
     return False
   return True
