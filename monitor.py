7959793ba08ebd208deded1cd61427ef69e7808d
Shivram-S <44567173+Shivram-S@users.noreply.github.com>
2025-07-16 15:15:04 +0530

    Update monitor.py - Removed for loop

--- monitor.py
+++ monitor.py
@@ -8,14 +8,7 @@ def isInRange(low,up,quantity):
   return False
 
 def vitals_ok(temperature, pulseRate, spo2):
-  if (not isInRange(95,102,temperature)) or (not isInRange(60,100,pulseRate))  or  spo2 < 90:
+  if (not isInRange(95,102,temperature)) or (not isInRange(60,100,pulseRate))  or (not isInRange(90,1000,spo2)):
     print('Vitals critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
     return False
   return True
