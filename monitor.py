17397c5962e1baee817cad0299390c21bdd702ad
Shivram-S <44567173+Shivram-S@users.noreply.github.com>
2025-07-16 15:50:02 +0530

    Update monitor.py - Use of flag

--- monitor.py
+++ monitor.py
@@ -8,13 +8,8 @@ def isNotInRange(low,up,quantity):
   return False
 
 def vitals_ok(temperature, pulseRate, spo2):
-  if isNotInRange(95,102,temperature):
-    print('Temperature critical!')
-    return False
-  if isNotInRange(60,100,pulseRate):
-    print('Pulse critical!')
-    return False
-  if spo2<90:
-    print('SPO2 critical!')
-    return False
-  return True
+    flag = True
+    if isNotInRange(95,102,temperature) or isNotInRange(60,100,pulseRate) or spo2<90:
+        print('Temperature critical!')
+        flag = False
+    return flag
