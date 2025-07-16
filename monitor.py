6cd479d6ee12363189297adad9acfe10f9886827
Shivram-S <44567173+Shivram-S@users.noreply.github.com>
2025-07-16 14:51:40 +0530

    Update monitor.py - TRying to reduce CCN

--- monitor.py
+++ monitor.py
@@ -4,28 +4,8 @@ import sys
 
 
 def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
+  if temperature > 102 or temperature < 95 or pulseRate < 60 or pulseRate > 100 or spo2 < 90:
+    print('Vitals critical!')
     for i in range(6):
       print('\r* ', end='')
       sys.stdout.flush()
