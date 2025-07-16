4f13fb4813d8e31b0497b92bf5ae391aa62bbc19
Shivram-S <44567173+Shivram-S@users.noreply.github.com>
2025-07-16 15:09:08 +0530

    Update monitor.py - Trying with function

--- monitor.py
+++ monitor.py
@@ -2,9 +2,13 @@
 from time import sleep
 import sys
 
+def isInRange(low,up,quantity):
+  if quantity <= up and quantity >= low:
+    return True
+  return False
 
 def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95 or pulseRate < 60 or pulseRate > 100 or spo2 < 90:
+  if !(isInRange(95,102,temperature)) or (!isInRange(60,100,pulseRate))  or spo2 < 90:
     print('Vitals critical!')
     return False
   return True
