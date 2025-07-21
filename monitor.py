54bb6168654463a76ad6a44394a5f4b1b9b768c6
Shivram-S <44567173+Shivram-S@users.noreply.github.com>
2025-07-21 16:50:38 +0530

    Update monitor.py - Removal of If

--- monitor.py
+++ monitor.py
@@ -2,14 +2,11 @@
 from time import sleep
 import sys
 
-def isNotInRange(low,up,quantity):
-  if quantity > up or quantity < low:
+def isInRange(low,up,quantity):
+  if quantity <= up and quantity >= low:
     return True
   return False
 
 def vitals_ok(temperature, pulseRate, spo2):
-    flag = True
-    if isNotInRange(95,102,temperature) or isNotInRange(60,100,pulseRate) or spo2<90:
-        print('Temperature critical!')
-        flag = False
+    flag = isInRange(95,102,temperature) and isInRange(60,100,pulseRate) and spo2>=90
     return flag
